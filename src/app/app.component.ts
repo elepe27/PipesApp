ec47b66530bca8829ff35275a99d7b44fc8e9e88
Esteban Lepe <esteban.lepe1c@gmail.com>
2021-04-13 18:22:36 -0400

    Introducción a los pipes de Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  
+  nombre: string = 'esteban lepe'
+  valor: number = 15234;
+  obj = {
+    nombre : 'esTeban eDuaRdo lEpE CaStRo'
+  }
+  mostrarNombre(){
+    console.log(this.nombre);
+    console.log(this.valor);
+  }
 }
